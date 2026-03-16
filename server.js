89afcd6296eaaff64c33446ab75d0b65a09bd8ff
Naman Jain <progemer2007128@gmail.com>
2026-03-16 19:57:49 +0530

    backend chatv1

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+const rooms = {};
+
+io.on("connection", (socket) => {
+
+  socket.on("join-room", ({ roomId, username }) => {
+
+    socket.join(roomId);
+    socket.roomId = roomId;
+    socket.username = username;
+
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    rooms[roomId].push(socket.id);
+
+    io.to(roomId).emit("system-message", `${username} joined the room`);
+
+  });
+
+  socket.on("send-message", (data) => {
+
+    io.to(data.roomId).emit("receive-message", {
+      username: socket.username,
+      message: data.message
+    });
+
+  });
+
+  socket.on("disconnect", () => {
+
+    const roomId = socket.roomId;
+
+    if (!roomId) return;
+
+    rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
+
+    io.to(roomId).emit(
+      "system-message",
+      `${socket.username} left the room`
+    );
+
+    if (rooms[roomId].length === 0) {
+      delete rooms[roomId];
+    }
+
+  });
+
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
